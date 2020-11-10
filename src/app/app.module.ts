4c0c3e5d1f52324cd9b2ae385259ef9e041ad170
Thessalène <thessalene.jeanlouis@reseau.eseo.fr>
2020-11-10 13:54:44 -0400

    Body of the app (dashboard and different links)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,59 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule} from '@angular/material/toolbar';
+import { MatIconModule} from '@angular/material/icon';
+import { MatSidenavModule} from '@angular/material/sidenav';
+import { MatListModule} from '@angular/material/list';
+import { MatButtonModule} from '@angular/material/button';
+import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from '../app/dashboard/dashboard.component';
+import { EmployeeListComponent } from '../app/employee-list/employee-list.component';
+import { NavigationComponent } from '../app/navigation/navigation.component';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import {MatGridListModule} from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { LayoutModule } from '@angular/cdk/layout';
+import { TreeComponent } from './tree/tree.component';
+import { MatTreeModule } from '@angular/material/tree';
+import { DragDropComponent } from './drag-drop/drag-drop.component';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+
+const routes : Routes = [
+  { path : '', component : DashboardComponent},
+  { path : 'dashboard', component : DashboardComponent},
+  { path : 'tree', component : TreeComponent},
+  { path : 'dragdrop', component : DragDropComponent},
+  { path : 'list', component : EmployeeListComponent},
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, DashboardComponent, NavigationComponent, EmployeeListComponent, TreeComponent, DragDropComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    MatToolbarModule, 
+    MatIconModule, 
+    MatSidenavModule, 
+    MatListModule, 
+    MatButtonModule,
+    MatPaginatorModule,
+    MatGridListModule,
+    RouterModule.forRoot(routes),
+    MatCardModule,
+    MatMenuModule,
+    LayoutModule,
+    MatTreeModule,
+    DragDropModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/employee-list/employee-list.component.ts
@@ -0,0 +1,30 @@
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTable } from '@angular/material/table';
+import { EmployeeListDataSource, EmployeeListItem } from './employee-list-datasource';
+
+@Component({
+  selector: 'app-employee-list',
+  templateUrl: './employee-list.component.html',
+  styleUrls: ['./employee-list.component.scss']
+})
+export class EmployeeListComponent implements AfterViewInit, OnInit {
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatTable) table: MatTable<EmployeeListItem>;
+  dataSource: EmployeeListDataSource;
+
+  /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
+  displayedColumns = ['id', 'name'];
+
+  ngOnInit() {
+    this.dataSource = new EmployeeListDataSource();
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+    this.table.dataSource = this.dataSource;
+  }
+}
